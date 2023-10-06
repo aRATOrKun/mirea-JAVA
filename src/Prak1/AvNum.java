5365bd12f721646019b7a9d924121822d852b4b9
aRATOrKun <misha121323@gmail.com>
2023-10-06 19:10:17 +0300

    Add files

--- src/Prak1/AvNum.java
+++ src/Prak1/AvNum.java
@@ -1,2 +1,41 @@
-package Prak1;public class AvNum {
+package Prak1;
+import java.util.Scanner;
+
+public class AvNum
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        // Ввести размер массива
+        System.out.print("Введите размер массива: ");
+        int size = scanner.nextInt();
+
+        // Создать массив
+        int[] numbers = new int[size];
+
+        // Вводим данные
+        System.out.println("Введите элементы массива:");
+        for (int i = 0; i < size; i++)
+        {
+            System.out.print("Элемент " + (i + 1) + ": ");
+            numbers[i] = scanner.nextInt();
+        }
+
+        // Сумма Элементов
+        int sum = 0;
+        for (int i = 0; i < size; i++)
+            sum += numbers[i];
+
+
+        // Среднее арифметическое
+        double average = (double) sum / size;
+
+        // Выводим результаты
+        System.out.println("Сумма элементов массива: " + sum);
+        System.out.println("Среднее арифметическое: " + average);
+
+        // Закрываем Scanner
+        scanner.close();
+    }
 }
