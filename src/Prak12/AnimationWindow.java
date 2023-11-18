4b557114c9fd725049aa7be808eff1915fa6633c
aRATOr <misha121323@gmail.com>
2023-11-18 10:16:36 +0300

    Add files

--- src/Prak12/AnimationWindow.java
+++ src/Prak12/AnimationWindow.java
@@ -3,35 +3,31 @@ package Prak12;
 import javax.swing.*;
 import java.awt.*;
 
-public class AnimationWindow extends JFrame {
+public class AnimationWindow extends JFrame
+{
 
     private Image[] frames;
     private int currentFrame;
     private Timer animationTimer;
 
-    public AnimationWindow() {
-        // Создаем массив изображений
+    public AnimationWindow()
+    {
         frames = new Image[4];
         frames[0] = Toolkit.getDefaultToolkit().getImage("frame1.png");
         frames[1] = Toolkit.getDefaultToolkit().getImage("frame2.png");
         frames[2] = Toolkit.getDefaultToolkit().getImage("frame3.png");
         frames[3] = Toolkit.getDefaultToolkit().getImage("frame4.png");
 
-        // Инициализируем текущий кадр
         currentFrame = 0;
 
-        // Создаем таймер для анимации
-        animationTimer = new Timer(100, e -> {
-            // Переключаемся на следующий кадр
+        animationTimer = new Timer(100, e ->
+        {
             currentFrame = (currentFrame + 1) % frames.length;
-            // Перерисовываем окно
             repaint();
         });
 
-        // Запускаем таймер
         animationTimer.start();
 
-        // Настраиваем окно
         setTitle("Анимация");
         setSize(400, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -39,13 +35,14 @@ public class AnimationWindow extends JFrame {
     }
 
     @Override
-    public void paint(Graphics g) {
+    public void paint(Graphics g)
+    {
         super.paint(g);
-        // Отрисовываем текущий кадр на окне
         g.drawImage(frames[currentFrame], 0, 0, null);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SwingUtilities.invokeLater(AnimationWindow::new);
     }
 }
